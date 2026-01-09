cef3c1ed2e7dbbf1d592eef544e825d8986ae831
hesilovamedine-bit <hesilova.medine@gmail.com>
2026-01-09 16:15:01 +0400

    pustokellaaa

--- pustok/obj/Debug/net8.0/pustok.AssemblyInfo.cs
+++ pustok/obj/Debug/net8.0/pustok.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pustok")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c2528bbfdc04c6f92b8ab075965ee970e6e25f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5e7981e275892025c11ebca162ea540f88eebe")]
 [assembly: System.Reflection.AssemblyProductAttribute("pustok")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pustok")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
